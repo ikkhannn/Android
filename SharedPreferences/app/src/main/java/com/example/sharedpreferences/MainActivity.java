466e5ed7082781a2ecf67f3dd07a227479e0ace0
ikkhannn <imrankhannn81@gmail.com>
2019-05-30 23:06:40 +0500

    added shared preferences

---
+++ SharedPreferences/app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.sharedpreferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.result_text_view);
+
+        loadData();
+        updateViews();
+    }
+
+    public void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(TEXT, textView.getText().toString());
+
+        editor.apply();
+
+        Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show();
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        text = sharedPreferences.getString(TEXT, "");
+    }
+
+    public void updateViews() {
+        textView.setText(text);
+    }
+
+
+    }
+
+
+
+
+
+
+}
